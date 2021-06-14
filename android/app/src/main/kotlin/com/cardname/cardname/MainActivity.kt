57059441e5ade2ba08d33b4de03e643696d49937
Rizky Fauzan <rizky.fauzan2408@gmail.com>
2021-06-14 12:00:12 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/cardname/cardname/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cardname.cardname
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: MyCard(),
+));
+
+class MyCard extends StatefulWidget {
+
+  @override
+  _MyCardState createState() => _MyCardState();
+}
+
+class _MyCardState extends State<MyCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text('My ID Card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 8.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.40, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/images/banagher.jpg'),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[800],
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              )
+            ),
+            SizedBox(height: 10.0),
+            Text(
+                'Banagher Links',
+                style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2.0,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                )
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              'CURRENT LEVEL',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              )
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              '8',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              )
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'banagher.links@mail.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  )
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
